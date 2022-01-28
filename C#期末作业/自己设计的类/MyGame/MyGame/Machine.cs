4f5da67818386e149d618ec174fc91011c437973
SongJie-Hush <songjie.hush@gmail.com>
2022-01-28 20:50:10 +0800

    Add files via upload

---
+++ C#期末作业/自己设计的类/MyGame/MyGame/Machine.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    public class Machine
+    {
+            int i;
+            public void Begin()  //启动
+            {
+                Console.WriteLine("Welcome to the Game created by Song Jie!\n");
+                Console.WriteLine("************************************************\n");
+            }
+
+            public void  FirstAsk()//第一次问是否游戏
+            {
+                Console.WriteLine("Your magic adventure starts now!");
+                Console.WriteLine();
+            }
+
+            public bool Ask()  //判断是否中断游戏
+            {
+                Console.WriteLine("\nPress 0 to exit:");
+                Console.WriteLine("Press any others to continue:");
+                Console.WriteLine();
+                string sop = Console.ReadLine();
+                try
+                {
+                    i = Int32.Parse(sop);
+                }
+                catch
+                {
+                    i = 1;
+                }
+
+                if (i != 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Hoping for your next adventure!");
+                    return false;
+                }
+            }
+
+        }
+    }
